652da7ea0f83cf4222c9ece15a90d5e94a29565a
Nyein Chan Aung <nyeinchanaung@koekoetech.com>
2022-01-08 23:08:08 +0630

    End of section 8

--- client-app/src/features/activities/ActivityList.tsx
+++ client-app/src/features/activities/ActivityList.tsx
@@ -1,11 +1,12 @@
 import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
-  const { selectActivity, deleteActivity, activitiesByDate, loading } = activityStore;
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
   const [target, setTarget] = useState("");
 
   function handleActivityDelete(
@@ -32,7 +33,8 @@ export default observer(function ActivityList() {
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
+                  as={Link}
+                  to={`/activities/${activity.id}`}
                   floated="right"
                   content="View"
                   color="blue"
